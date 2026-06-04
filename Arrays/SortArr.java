bb7f712e3543b0b06429f603a9ffc70ddc5cf25d
polasa-swathi <polasaswathi2004@gmail.com>
2026-06-04 18:21:57 +0530

    Create SortArr.java
    Sorts the elements of an array in ascending order using java's built-in Arrays.sort() method.

---
+++ Arrays/SortArr.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sortArr {
+    public static void main(String[] args){
+        int arr[]={1,5,3,7,2,4,9,8};
+        Arrays.sort(arr);
+        for(int x: arr){
+            System.out.print(x+" ");
+        }
+    }
+}
