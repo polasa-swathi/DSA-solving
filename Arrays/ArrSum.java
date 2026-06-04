2f9a1d4964cec1f8ee7a5576ab79c0a7f43c24f5
polasa-swathi <polasaswathi2004@gmail.com>
2026-06-04 18:18:04 +0530

    Create ArrSum.java
    Finds the sum of all elements in an array using a simple loop in java.

---
+++ Arrays/ArrSum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class sumArr {
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5};
+        int sum=0;
+        for(int x : arr){
+            sum+=x;
+        }
+        System.out.println(sum);
+    }
+    
+}
