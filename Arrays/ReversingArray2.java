a78fd557e66784309cbe1c838566e71382cfaceb
polasa-swathi <polasaswathi2004@gmail.com>
2026-06-04 18:35:29 +0530

    Create ReversingArray2.java
    This Java program reverses an array by swapping elements using a temporary variable.

---
+++ Arrays/ReversingArray2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class reverseArr {
+
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5};
+        int j=arr.length-1;
+      for(int i=0;i<arr.length/2;i++){
+           int temp=arr[i];
+           arr[i]=arr[j];
+           arr[j]=temp;
+           j--;
+      }
+      for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+      }
+    }
+}
